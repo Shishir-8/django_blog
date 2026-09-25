870a35a6a02bc3d49ccfe75ea2a0d42d1a1e15fb
Shishir Oli <shishir@Shishirs-MacBook-Air.local>
2026-09-25 08:43:36 +0545

    feat: added base.html

--- blogs/admin.py
+++ blogs/admin.py
@@ -9,6 +9,7 @@ admin.site.register(Category)
 @admin.register(Blog)
 class BlogAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug': ('title',)}
-    list_display = ['title', 'category', 'author', 'status']
+    list_display = ['title', 'category', 'author', 'status', 'is_featured']
     search_fields = ['id', 'title']
+    list_filter = ['category']
 
--- blogs/views.py
+++ blogs/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Blog, Category
+
+
 
 # Create your views here.
+
+def posts_by_category(request, category_id):
+    category = get_object_or_404(Category, id=category_id)
+    posts = Blog.objects.filter(status='published', category=category)
+    context = {
+        'posts': posts,
+        'category': category,
+
+    }
+    return render(request, 'post_by_category.html', context)
+
+
+def blogs(request, slug):
+    return render(request, 'blogs.html')
